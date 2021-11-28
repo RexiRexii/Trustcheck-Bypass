bacae27fa42ecfac2413e96c63856f500b3bd7bb
Rexi <68111233+RexiRexii@users.noreply.github.com>
2021-11-28 21:42:16 +0300

    Update bypass.hpp

--- bypass.hpp
+++ bypass.hpp
@@ -7,3 +7,5 @@
 		*reinterpret_cast<byte*>(TRUSTCHECKJNZ) = 0xEB;
 		VirtualProtect(reinterpret_cast<LPVOID>(TRUSTCHECKJNZ), 1, EXECUTE, &EXECUTE);
 	}
+// usage: literally just call InitializeTrustcheck() xddd
+// REQUIRES A MEMCHECK BYPASS, I SUGGEST YOU USE GOGO'S AUTOUPDATING MEMCHECK BYPASS AT: https://github.com/AmJayden/auto-updating-memcheck/tree/main/AUMB
