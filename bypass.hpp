0453ebffc064e968b60e937c6491430ab8364c97
Rexi <68111233+RexiRexii@users.noreply.github.com>
2021-11-28 21:44:31 +0300

    Update bypass.hpp

--- bypass.hpp
+++ bypass.hpp
@@ -1,11 +1,11 @@
-	void InitializeTrustcheck()
-	{
-		DWORD EXECUTE;
-		const auto TRUSTCHECKJNZ = aslr(0x182194F);
+void InitializeTrustcheck()
+{
+	DWORD EXECUTE;
+	const auto TRUSTCHECKJNZ = aslr(0x182194F);
 
-		VirtualProtect(reinterpret_cast<LPVOID>(TRUSTCHECKJNZ), 1, PAGE_EXECUTE_READWRITE, &EXECUTE);
-		*reinterpret_cast<byte*>(TRUSTCHECKJNZ) = 0xEB;
-		VirtualProtect(reinterpret_cast<LPVOID>(TRUSTCHECKJNZ), 1, EXECUTE, &EXECUTE);
-	}
+	VirtualProtect(reinterpret_cast<LPVOID>(TRUSTCHECKJNZ), 1, PAGE_EXECUTE_READWRITE, &EXECUTE);
+	*reinterpret_cast<byte*>(TRUSTCHECKJNZ) = 0xEB;
+	VirtualProtect(reinterpret_cast<LPVOID>(TRUSTCHECKJNZ), 1, EXECUTE, &EXECUTE);
+}
 // usage: literally just call InitializeTrustcheck() xddd
 // REQUIRES A MEMCHECK BYPASS, I SUGGEST YOU USE GOGO'S AUTOUPDATING MEMCHECK BYPASS AT: https://github.com/AmJayden/auto-updating-memcheck/tree/main/AUMB
