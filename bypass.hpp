79ecdebb0f43b8b9275b54b8694e111d6e0b7a56
Rexi <68111233+RexiRexii@users.noreply.github.com>
2021-11-29 04:43:36 +0300

    Update bypass.hpp
    added a revert option

--- bypass.hpp
+++ bypass.hpp
@@ -7,5 +7,15 @@ void InitializeTrustcheck()
 	*reinterpret_cast<byte*>(TRUSTCHECKJNZ) = 0xEB;
 	VirtualProtect(reinterpret_cast<LPVOID>(TRUSTCHECKJNZ), 1, EXECUTE, &EXECUTE);
 }
-// usage: literally just call InitializeTrustcheck() xddd
+
+void RevertTrustcheck()
+{
+	DWORD EXECUTE;
+	const auto TRUSTCHECKJNZ = (0x182194F - 0x400000 + reinterpret_cast<std::uintptr_t>(GetModuleHandleA(0)));
+
+	VirtualProtect(reinterpret_cast<void*>(TRUSTCHECKJNZ), 1, PAGE_EXECUTE_READWRITE, &EXECUTE);
+	*reinterpret_cast<byte*>(TRUSTCHECKJNZ) = 0x74;
+	VirtualProtect(reinterpret_cast<void*>(TRUSTCHECKJNZ), 1, EXECUTE, &EXECUTE);
+}
+
 // REQUIRES A MEMCHECK BYPASS, I SUGGEST YOU USE GOGO'S AUTOUPDATING MEMCHECK BYPASS AT: https://github.com/AmJayden/auto-updating-memcheck/tree/main/AUMB
