eb83af6d0e384e9e0003daa3e661e55059626585
Rexi <68111233+RexiRexii@users.noreply.github.com>
2021-12-06 14:40:50 +0300

    Update bypass.hpp
    updated for version-3f8c74d282f8480f

--- bypass.hpp
+++ bypass.hpp
@@ -1,7 +1,7 @@
 void InitializeTrustcheck()
 {
 	DWORD EXECUTE;
-	const auto TRUSTCHECKJNZ = (0x182194F - 0x400000 + reinterpret_cast<std::uintptr_t>(GetModuleHandleA(0)));
+	const auto TRUSTCHECKJNZ = (0x1ABE4AF - 0x400000 + reinterpret_cast<std::uintptr_t>(GetModuleHandleA(0)));
 
 	VirtualProtect(reinterpret_cast<void*>(TRUSTCHECKJNZ), 1, PAGE_EXECUTE_READWRITE, &EXECUTE);
 	*reinterpret_cast<byte*>(TRUSTCHECKJNZ) = 0xEB;
@@ -11,7 +11,7 @@ void InitializeTrustcheck()
 void RevertTrustcheck()
 {
 	DWORD EXECUTE;
-	const auto TRUSTCHECKJNZ = (0x182194F - 0x400000 + reinterpret_cast<std::uintptr_t>(GetModuleHandleA(0)));
+	const auto TRUSTCHECKJNZ = (0x1ABE4AF - 0x400000 + reinterpret_cast<std::uintptr_t>(GetModuleHandleA(0)));
 
 	VirtualProtect(reinterpret_cast<void*>(TRUSTCHECKJNZ), 1, PAGE_EXECUTE_READWRITE, &EXECUTE);
 	*reinterpret_cast<byte*>(TRUSTCHECKJNZ) = 0x74;
